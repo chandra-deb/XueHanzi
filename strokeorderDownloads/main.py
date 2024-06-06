ad60c9eaa669d691eabe6b1fbc9aea1bea7eace7
chandra-deb <mail.chandradeb@gmail.com>
2024-06-06 23:44:21 +0800

    ...

---
+++ strokeorderDownloads/main.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup
+import requests
+import hsk1, hsk2, hsk3, hsk4, hsk5, hsk6
+import os
+
+
+def downloadCharImages(words):
+    charDownloadCount = 0
+    sheetDownloadCount = 0
+    for word in words:
+        whole_word = word["hanzi"]
+
+        urls = []
+        characters = []
+
+        for single in whole_word:
+            if single not in characters:
+                characters.append(single)
+
+                if os.path.exists(f"./hsk6/{single}.png"):
+                    charDownloadCount += 1
+                    print(f"Exists: {single} -> {charDownloadCount}")
+                if os.path.exists(f"./hsk6/sheets/{single}-sheet.png"):
+                    sheetDownloadCount += 1
+                    print(f"Sheet Exists: {single} -> {sheetDownloadCount}")
+                else:
+
+                    url = f"https://www.strokeorder.com/chinese/{single}"
+                    response = requests.get(url)
+                    soup = BeautifulSoup(response.text, "html.parser")
+
+                    div = soup.find_all("div", class_="stroke-article-content")
+                    div_sheet = div[1]
+                    div_char = div[0]
+                    if div_char is not None:
+                        img = div_char.find("img")
+                        if img is not None:
+                            img_url = f"https://www.strokeorder.com{img['src']}"
+                            if os.path.exists(f"./hsk6/{single}.png"):
+                                print(f"File already exists for character: {single}")
+                            else:
+                                response = requests.get(img_url)
+                                if response.status_code == 200:
+                                    with open(f"./hsk6/{single}.png", "wb") as f:
+                                        f.write(response.content)
+                                    charDownloadCount += 1
+                                    print("charDownloaded: " + str(charDownloadCount))
+                                else:
+                                    print(
+                                        f"Failed to download image for character: {single}"
+                                    )
+                            response = requests.get(img_url)
+                            if response.status_code == 200:
+                                with open(f"./hsk6/{single}.png", "wb") as f:
+                                    f.write(response.content)
+                                charDownloadCount += 1
+                                print("charDownloaded: " + str(charDownloadCount))
+                            else:
+                                print(
+                                    f"Failed to download image for character: {single}"
+                                )
+                    # for character sheet
+                    if div_sheet is not None:
+                        img = div_sheet.find("img")
+                        if img is not None:
+                            img_url = f"https://www.strokeorder.com{img['src']}"
+                            response = requests.get(img_url)
+                            if response.status_code == 200:
+                                with open(
+                                    f"./hsk6/sheets/{single}-sheet.png", "wb"
+                                ) as f:
+                                    f.write(response.content)
+                                urls.append(img_url)
+                                sheetDownloadCount += 1
+                                print("sheetDownloaded: " + str(sheetDownloadCount))
+                            else:
+                                print(
+                                    f"Failed to download image for character: {single}"
+                                )
+        # return render_template('writing_practice.html',
+        #                     title='Practice' ,
+        #                     img_urls=urls, character=character)
+
+
+downloadCharImages(hsk6.words)
